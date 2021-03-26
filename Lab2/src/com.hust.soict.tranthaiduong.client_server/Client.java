6ed9d809c236611a9c30f519ec1d86d3471b7d96
ImDuong <shiroyasha699969@gmail.com>
2021-03-26 12:09:03 +0700

    init lab2

---
+++ Lab2/src/com.hust.soict.tranthaiduong.client_server/Client.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[]args){
+        Socket socket=new Socket("127.0.0.1",9696);
+        BufferedReader in=new BufferedReader(new
+                InputStreamReader(socket.getInputStream()));
+        PrintWriter out=new
+                PrintWriter(socket.getOutputStream(),true);
+        System.out.println(in.readLine());
+        Scanner scanner=new Scanner(System.in);
+        String message=scanner.nextLine();
+        socket.close();
+        scanner.close();
+    }
+}
